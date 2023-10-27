58294b45a72a0eb62c0eec9b7a95d8fce8b5e87a
sudiasali <sudaisali420@gmail.com>
2023-10-27 11:57:31 +0500

    print

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ import * as fun from './functions.js'
 
 let contactdetails = new ContactDetails("sialkot","Punjab","03185097377","AirportRoad Gohdpur")
 let user1 = new User("1","sduais","23","2000",12345,contactdetails)
+const userData = user1
 console.log(user1)
 fun.createUser(user1)
+console.table(Json)
